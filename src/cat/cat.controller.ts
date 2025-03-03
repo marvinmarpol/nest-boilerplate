05cd0a2b44a7a6f697e29b08b491b450597b348e
unknown <marvinmarpol@gmail.com>
2025-03-03 14:12:14 +0700

    feat: add basic example for controllers

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,10 +1,56 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDTO } from './cat.create.dto';
 
 @Controller('cat')
 export class CatController {
+  @Post()
+  @Header('Cache-Control', 'no-store')
+  @HttpCode(204)
+  create(@Body() createDTO: CreateCatDTO): string {
+    return 'create a cat';
+  }
+
   @Get()
   findAll(@Req() request: Request): string {
     return 'all cats';
+  }
+
+  @Get('/q')
+  async findAllWQuery(
+    @Query('age') age: number,
+    @Query('breed') breed: string,
+  ) {
+    return `This action returns all cats filtered by age: ${age} and breed: ${breed}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Get('/abcd/*')
+  @Redirect('https://nestjs.com', 301)
+  findWildCat(): string {
+    return 'This uses a wildcard';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
